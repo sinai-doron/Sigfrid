ca00993bac06ace1c3012f74156bf5151f53ca52
Doron sinai <doron.sinai@convertro.com>
2014-10-23 19:35:37 +0300

    add folder creation

---
+++ createFolder.js
@@ -0,0 +1,35 @@
+/**
+ * Created by doron on 10/23/14.
+ */
+var mongoose = require('mongoose');
+var config = require('config');
+var downloadFolder = config.get('downloadFolder');
+var fs = require('fs');
+var _ = require('lodash');
+require("./db.js");
+
+var User = mongoose.model('User');
+var Show = mongoose.model('Show');
+var Episode = mongoose.model('Episode');
+
+function checkAndCreateFolder(folder){
+    if(fs.existsSync(folder)){
+        return true;
+    }
+    else{
+        fs.mkdirSync(folder);
+        if(fs.existsSync(folder)){
+            return true;
+        }
+        return false;
+    }
+}
+
+Show.find({},function(err, shows){
+    _.each(shows,function(show){
+        var name = show.name;
+        checkAndCreateFolder(downloadFolder + name + '/');
+    });
+});
+
+process.exit(1);
